7bc780b0b003f1b3913926df1c5fcf614a183ac1
pascalaubry <pascalaubry35@gmail.com>
2024-12-22 09:17:34 +0100

    Added comments to new methods

--- src/data/event.py
+++ src/data/event.py
@@ -367,6 +367,7 @@ class Event:
         }
 
     def get_unused_tournament_uniq_id(self, base_uniq_id: str) -> str:
+        """ Returns the first unused tournament uniq_id (base_uniq_id, or base_uniq_id-2, or base_uniq_id-n+1... """
         index: int
         uniq_id: str
         if matches := re.match(r'^(.*)-(\d+)$', base_uniq_id):
@@ -382,6 +383,7 @@ class Event:
         return uniq_id
 
     def get_unused_tournament_name(self, base_name: str) -> str:
+        """ Returns the first unused tournament name (base_name, or base_name (2), or base_name (n+1)... """
         index: int
         name: str
         if matches := re.match(r'^(.*) \((\d+)\)$', base_name):
