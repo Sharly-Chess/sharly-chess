82a3e7c9372f3a83d8bd21af7125b275f1d1721a
pascalaubry <pascalaubry35@gmail.com>
2025-02-06 20:36:51 +0100

    Update generate_papi_template.py instead of papi_template.py

--- utils/papi/generate_papi_template.py
+++ utils/papi/generate_papi_template.py
@@ -8,7 +8,7 @@ import base64
 from pathlib import Path
 
 script_dir: Path = Path() / 'utils' / 'papi'
-output_dir = Path() / 'src' / 'database'
+output_dir = Path() / 'src'/ 'database' / 'access' / 'papi'
 
 papi_versions = [
     path.stem.replace('template-', '').replace('.papi', '')
@@ -56,15 +56,12 @@ with open(output_dir / 'papi_template.py', 'wt', encoding='utf-8') as output_fil
     output_file.write('        case _:\n            raise ValueError()\n')
     output_file.write(
         '    if not file.parents[0].is_dir():\n'
-        "        logger.warning(f'Directory [%s] not found, could not generate the Papi file.', file.parents[0])\n"
-        '        return False\n'
-        "    with open(file, 'wb') as f:\n"
-        '        f.write(\n'
-        '            bz2.decompress(\n'
-        '                base64.decodebytes(\n'
-        '                    b64\n'
-        '                )\n'
-        '            )\n'
+        '        logger.warning(\n'
+        '            \'Directory [%s] not found, could not generate the Papi file.\',\n'
+        '            file.parents[0],\n'
         '        )\n'
+        '        return False\n'
+        '    with open(file, \'wb\') as f:\n'
+        '        f.write(bz2.decompress(base64.decodebytes(b64)))\n'
         '    return True\n'
     )
