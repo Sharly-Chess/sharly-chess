c06ef29cc7746c9f0a157b8e923e50ffce4b4d6d
Molrn <76754000+Molrn@users.noreply.github.com>
2025-03-05 15:03:48 +0100

    fix comment

--- src/data/tournament.py
+++ src/data/tournament.py
@@ -936,7 +936,7 @@ class Tournament:
                 f'(last finished round: [{self.max_ranking_round}])'
             )
         max_round = max_round or self.max_ranking_round or self.rounds
-        # Estimate pairings to ensure we have a defined rank for everyone
+        # Estimate rankings to ensure we have a defined rank for everyone
         self.estimate_players(max_round=max_round, papi_legacy=papi_legacy)
         for player in self.players_by_id.values():
             player.points = (
