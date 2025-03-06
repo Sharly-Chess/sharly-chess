e61df5a6f5651bf88523974b086311c99a957f1b
Timothy Armes <tim@timothyarmes.com>
2025-03-06 19:58:15 +0100

    Move the category back to the core.  Brain fart.

--- src/data/util.py
+++ src/data/util.py
@@ -1302,6 +1302,7 @@ def performance_bonus(
 
 class PrintSplit(StrEnum):
     NO_SPLIT = 'no-split'
+    CATEGORY = 'category'
     CLUB = 'club'
     FEDERATION = 'federation'
 
@@ -1310,6 +1311,8 @@ class PrintSplit(StrEnum):
         match self:
             case PrintSplit.NO_SPLIT:
                 return _('No split')
+            case PrintSplit.CATEGORY:
+                return _('Category')
             case PrintSplit.CLUB:
                 return _('Club')
             case PrintSplit.FEDERATION:
