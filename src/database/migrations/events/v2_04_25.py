2cc034dc07c43dc793e4065e4fc8304b546cfe36
Sammy Plat <sammy@sammyplat.fr>
2025-03-07 21:50:04 +0100

    Removed useless migration

--- src/database/migrations/events/v2_04_25.py
+++
@@ -1,9 +0,0 @@
-from database.sqlite.event_migration import AbstractEventMigration
-
-
-class EventMigration(AbstractEventMigration):
-    def forward(self):
-        pass
-
-    def backward(self):
-        pass
