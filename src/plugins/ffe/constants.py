213423af0ccdcd425a84c62697d853ffb37ffa39
Molrn <76754000+Molrn@users.noreply.github.com>
2025-03-12 14:13:14 +0100

    aply suggestion

--- src/plugins/ffe/constants.py
+++ src/plugins/ffe/constants.py
@@ -1,6 +1,6 @@
-#: Name of the plugin that will be referenced in our configuration
 from packaging.version import Version
 
+#: Name of the plugin that will be referenced in our configuration
 PLUGIN_NAME = "ffe"
 
 PLUGIN_VERSION = Version('0.1.0')
