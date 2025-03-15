f2421a5a2d56e280cd9c424c5345b5e71dc646d4
pascalaubry <pascalaubry35@gmail.com>
2025-03-15 11:17:41 +0100

    Fixed types in hookspec.py

--- src/plugins/hookspec.py
+++ src/plugins/hookspec.py
@@ -10,6 +10,7 @@ from common import APP_NAME
 from data.player import Player
 from data.tournament_export import AbstractTournamentExporter
 from data.util import PrintDocument, ScreenType
+from plugins.utils import AbstractPluginMigrationManager, PluginEngineArgument
 
 if TYPE_CHECKING:
     from data.tournament import Tournament
@@ -209,9 +210,9 @@ class AppHookSpecs:
         """Provide extra exporting formats for tournaments"""
 
     @hookspec
-    def get_event_migration_manager(self) -> 'AbstractPluginMigrationManager':
+    def get_event_migration_manager(self) -> AbstractPluginMigrationManager:
         """Provide a migration manager for event databases"""
 
     @hookspec
-    def get_engine_argument(self) -> 'PluginEngineArgument':
+    def get_engine_argument(self) -> PluginEngineArgument:
         """Provide an engine argument"""
