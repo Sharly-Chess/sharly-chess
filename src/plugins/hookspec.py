7dd0ae16d245b58b7e7e266af2e65af99e2f6218
Molrn <76754000+Molrn@users.noreply.github.com>
2025-03-19 18:49:49 +0100

    SharedUtils sytsem to override functions from plugins (#253)
    * SharedUtils sytsem to override functions from plugins
    
    * fix performance bonus return type
    
    * add cache for bonud_performance

--- src/plugins/hookspec.py
+++ src/plugins/hookspec.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterable, Callable
 from dataclasses import dataclass
+from decimal import Decimal
 from pathlib import Path
 from typing import NamedTuple, Any, TYPE_CHECKING
 
@@ -268,3 +269,15 @@ class AppHookSpecs:
     @hookspec
     def get_extra_tie_break_classes(self) -> list[type['AbstractTieBreak']]:
         """Provide extra tournament tie breaks"""
+
+    # ---------------------------------------------------------------------------------
+    # Shared utils
+    # ---------------------------------------------------------------------------------
+
+    @hookspec(firstresult=True)
+    def get_performance_bonus_function(self) -> Callable[[float], int | float]:
+        """Provide a function to compute the performance bonus"""
+
+    @hookspec(firstresult=True)
+    def get_round_ranking_function(self) -> Callable[[float | Decimal], int]:
+        """Provide a function to round a ranking to an integer"""
