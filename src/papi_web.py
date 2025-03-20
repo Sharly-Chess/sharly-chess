e0cb87a188c14242b1bb0e1a48010e7622a2e783
Molrn <76754000+Molrn@users.noreply.github.com>
2025-03-20 14:37:08 +0100

    Parse --path before other args in `papi_web.py`

--- src/papi_web.py
+++ src/papi_web.py
@@ -2,25 +2,36 @@ import argparse
 import os
 import sys
 from logging import Logger
+from typing import TYPE_CHECKING
 
 from common import DEVEL_ENV
 from common.i18n import _
 from common.logger import get_logger
-from plugins.manager import plugin_manager
-from plugins.utils import PluginEngineArgument
-from web.server_engine import ServerEngine
+
+if TYPE_CHECKING:
+    from plugins.utils import PluginEngineArgument
+
+
+# undocumented feature to start from a different folder and work with different configurations
+# Has to be executed before plugin_manager to avoid initializing from the wrong path
+path_parser = argparse.ArgumentParser(add_help=False)
+path_parser.add_argument('--path', default='.')
+args, remaining_args = path_parser.parse_known_args()
+os.chdir(args.path)
+
+from plugins.manager import plugin_manager  # Noqa: E402
+from web.server_engine import ServerEngine  # Noqa: E402
 
 logger: Logger = get_logger()
 
-
-plugin_engine_arguments: list[PluginEngineArgument] = (
-    plugin_manager.hook.get_engine_argument()
-)
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(parents=[path_parser])
 parser.add_argument(
     '-s', '--server', help='start the web server', action='store_true'
 )
 engine_argument_names: list[str] = ['server']
+plugin_engine_arguments: list['PluginEngineArgument'] = (
+    plugin_manager.hook.get_engine_argument()
+)
 for argument in plugin_engine_arguments:
     parser.add_argument(
         f'-{argument.flag}',
@@ -29,8 +40,6 @@ for argument in plugin_engine_arguments:
         action='store_true',
     )
     engine_argument_names.append(argument.name)
-# undocumented feature to start from a different folder and work with different configurations
-parser.add_argument('--path', default='.')
 if DEVEL_ENV:
     parser.add_argument(
         '-d',
@@ -38,7 +47,7 @@ if DEVEL_ENV:
         help='on the webserver, if there is an uncaught exception, drop to PDB',
         action='store_true',
     )
-args = parser.parse_args()
+args = parser.parse_args(remaining_args)
 
 if not any(getattr(args, name, False) for name in engine_argument_names):
     parser.print_help(sys.stderr)
@@ -49,7 +58,6 @@ if not any(getattr(args, name, False) for name in engine_argument_names):
         )
     )
     sys.exit(1)
-os.chdir(args.path)
 try:
     if args.server:
         se: ServerEngine = ServerEngine(debug=(DEVEL_ENV and args.debug))
