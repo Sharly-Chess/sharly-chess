65e03123e74a00a049d045ff96a40bf7379174d6
Timothy Armes <tim@timothyarmes.com>
2025-03-21 18:03:36 +0100

    Test the FFE password from the UI

--- src/plugins/ffe/ffe_event_controller.py
+++ src/plugins/ffe/ffe_event_controller.py
@@ -1,7 +1,13 @@
-from litestar import get
+from typing import Annotated, Any
+from litestar import get, post
 from litestar.response import Template
-from litestar_htmx import HTMXRequest, ClientRedirect
+from litestar_htmx import HTMXRequest, ClientRedirect, HTMXTemplate
+from litestar.enums import RequestEncodingType
+from litestar.params import Body
 
+from common.i18n import _
+from common.network import NetworkMonitor
+from plugins.ffe.engine.ffe_session import FFESession
 from plugins.ffe.ffe_session_handler import FFESessionHandler
 from plugins.ffe.util import PlayerFFELicence
 from web.controllers.admin.base_event_admin_controller import BaseEventAdminController, BaseEventAdminWebContext
@@ -9,7 +15,7 @@ from web.controllers.admin.player_admin_controller import PlayerAdminController
 
 
 class FfeAdminEventController(BaseEventAdminController):
-    
+
     @get(
         path='/ffe/event/{event_uniq_id:str}/players',
         name='ffe-admin-event-players-tab',
@@ -53,3 +59,42 @@ class FfeAdminEventController(BaseEventAdminController):
             request,
             event_uniq_id=event_uniq_id,
         )
+    
+    @post(
+        path='/ffe/test-password',
+        name='ffe-admin-tournament-test-auth',
+    )
+    async def htmx_admin_timer_clone(
+        self,
+        request: HTMXRequest,
+        data: Annotated[
+            dict[str, Any],
+            Body(media_type=RequestEncodingType.URL_ENCODED),
+        ],
+    ) -> Template | ClientRedirect:
+        ffe_auth_valid: bool | None = None
+
+        if NetworkMonitor.connected():
+            ffe_id = data['ffe_id']
+            ffe_password = data['ffe_password']
+            
+            if ffe_id and ffe_password:
+                ffe_auth_valid = FFESession(tournament=None, debug=False).test_auth(ffe_id=ffe_id, ffe_password=ffe_password)
+
+        errors = {}
+        # Compare to False, None means 'unable to check'
+        if ffe_auth_valid is False:
+            errors['ffe_id'] = _('Invalid FFE ID or password.')
+            errors['ffe_password'] = _('Invalid FFE ID or password.')
+
+        return HTMXTemplate(
+            template_name='ffe_tournament_ffe_auth_fields.html',
+            context={
+                'data': {
+                    'ffe_id': data['ffe_id'],
+                    'ffe_password': data['ffe_password'],
+                },
+                'ffe_auth_valid': ffe_auth_valid is True, 
+                'ffe_password_visible': data['ffe_password_visible'] == 'true',
+                'errors': errors,
+            })
