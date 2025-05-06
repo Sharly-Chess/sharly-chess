e6dda6fd47b49f78b1aa35baadffaa33d79a9989
Timothy Armes <tim@timothyarmes.com>
2025-05-06 11:22:15 +0200

    Use polyglot.js

--- src/web/templates/user/screen/timer.js
+++ src/web/templates/user/screen/timer.js
@@ -1,3 +1,19 @@
+var polyglot = new Polyglot({
+    locale: '{{ locale }}',
+    phrases: {
+        'seconds': '{{ _('<smart_count> second |||| <smart_count> seconds') }}',
+        'minutes': '{{ _('<smart_count> minute |||| <smart_count> minutes') }}',
+        'hours': '{{ _('<smart_count> hour |||| <smart_count> hours') }}',
+        'days': '{{ _('<smart_count> day |||| <smart_count> days') }}',
+        'weeks': '{{ _('<smart_count> week |||| <smart_count> weeks') }}',
+        'countdown': '{{ _('<first> and <second>') }}',
+    },
+    interpolation: {
+        prefix: '<',
+        suffix: '>',
+    },
+});
+
 {% with timer=screen.timer %}
 {% with color_1_r=timer.color_1_rgb.0, color_1_g=timer.color_1_rgb.1, color_1_b=timer.color_1_rgb.2 %}
 {% with color_2_r=timer.color_2_rgb.0, color_2_g=timer.color_2_rgb.1, color_2_b=timer.color_2_rgb.2 %}
@@ -40,75 +56,6 @@ var format = function (str, col) {
 String.prototype.format = function (col) {
     return format(this,col);
 }
-function weeks_days_hours_minutes_seconds_string(weeks, days, hours, minutes, seconds) {
-	switch (weeks) {
-	    case 0:
-            switch (days) {
-                case 0:
-                    switch (hours) {
-                        case 0:
-                            switch (minutes) {
-                                case 0:
-                                    switch (seconds) {
-                                        case 0:
-                                        case 1:
-                                            return "{{ _('{seconds} second') }}";
-                                        default:
-                                            return "{{ _('{seconds} seconds') }}";
-                                    }
-                                case 1:
-                                    switch (minutes) {
-                                        case 0: return "{{ _('{minutes} minute') }}";
-                                        case 1: return "{{ _('{minutes} minute and {seconds} second') }}";
-                                        default: return "{{ _('{minutes} minute and {seconds} seconds') }}";
-                                    }
-                                default:
-                                    switch (minutes) {
-                                        case 0: return "{{ _('{minutes} minutes') }}";
-                                        case 1: return "{{ _('{minutes} minutes and {seconds} second') }}";
-                                        default: return "{{ _('{minutes} minutes and {seconds} seconds') }}";
-                                    }
-                            }
-                        case 1:
-                            switch (minutes) {
-                                case 0: return "{{ _('{hours} hour') }}";
-                                case 1: return "{{ _('{hours} hour and {minutes} minute') }}";
-                                default: return "{{ _('{hours} hour and {minutes} minutes') }}";
-                            }
-                        default:
-                            switch (minutes) {
-                                case 0: return "{{ _('{hours} hours') }}";
-                                case 1: return "{{ _('{hours} hours and {minutes} minute') }}";
-                                default: return "{{ _('{hours} hours and {minutes} minutes') }}";
-                            }
-                    }
-                case 1:
-                    switch (hours) {
-                        case 0: return "{{ _('{days} day') }}";
-                        case 1: return "{{ _('{days} day and {hours} hour') }}";
-                        default: return "{{ _('{days} day and {hours} hours') }}";
-                    }
-                default:
-                    switch (hours) {
-                        case 0: return "{{ _('{days} days') }}";
-                        case 1: return "{{ _('{days} days and {hours} hour') }}";
-                        default: return "{{ _('{days} days and {hours} hours') }}";
-                    }
-            }
-	    case 1:
-            switch (days) {
-                case 0: return "{{ _('{weeks} week') }}";
-                case 1: return "{{ _('{weeks} week and {days} day') }}";
-                default: return "{{ _('{weeks} week and {days} days') }}";
-            }
-	    default:
-            switch (days) {
-                case 0: return "{{ _('{weeks} weeks') }}";
-                case 1: return "{{ _('{weeks} weeks and {days} day') }}";
-                default: return "{{ _('{weeks} weeks and {days} days') }}";
-            }
-	}
-}
 function duration_string(dur) {
 	seconds = dur % 60;
 	dur = (dur - seconds)/60;
@@ -118,19 +65,31 @@ function duration_string(dur) {
 	dur = (dur - hours)/24;
 	days = dur % 7;
 	weeks = (dur - days)/7;
-	return weeks_days_hours_minutes_seconds_string(
-	    weeks, days, hours, minutes, seconds
-    ).replace(
-        '{seconds}', seconds
-    ).replace(
-        '{minutes}', minutes
-    ).replace(
-        '{hours}', hours
-    ).replace(
-        '{days}', days
-    ).replace(
-        '{weeks}', weeks
-    );
+
+    if (weeks > 0) {
+        first = polyglot.t('weeks', weeks);
+        second = days > 0 ? polyglot.t('days', days) : undefined;
+    } else if (days > 0) {
+        first = polyglot.t('days', days);
+        second = hours > 0 ? polyglot.t('hours', hours) : undefined;
+    } else if (hours > 0) {
+        first = polyglot.t('hours', hours);
+        second = minutes > 0 ? polyglot.t('minutes', minutes) : undefined;
+    } else if (minutes > 0) {
+        first = polyglot.t('minutes', minutes);
+        second = seconds > 0 ? polyglot.t('seconds', seconds) : undefined;
+    } else {
+        first = polyglot.t('seconds', seconds);
+    }
+
+    if (second) {
+        return polyglot.t('countdown', {
+            first: first,
+            second: second,
+        });
+    }
+
+    return first;
 }
 function update_timer() {
 	now = new Date();
@@ -141,7 +100,6 @@ function update_timer() {
 	if (time < {{ timer_hour.timestamp_1 }}) { // {{ timer_hour.datetime_str_1 }} color_1 {{ timer_hour.text_before }}
 		color = 'rgb({{ color_1_r }},{{ color_1_g }},{{ color_1_b }})';
 		dur = duration_string({{ timer_hour.timestamp }} - time);
-		console.error('dur=' + dur)
 		text_html = '{{ timer_hour.text_before }}'.replace('%s', dur);
 		update_timer_values(clock_html, text_html, color);
 		return;
