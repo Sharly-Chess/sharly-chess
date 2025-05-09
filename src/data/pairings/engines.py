2100c50ec21ad6227398c0d8ef253c7954ddf515
Molrn <76754000+Molrn@users.noreply.github.com>
2025-05-09 10:34:56 +0200

    use dynamic Berger table generation + tests

--- src/data/pairings/engines.py
+++ src/data/pairings/engines.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import subprocess
+from functools import cache
 from pathlib import Path
 from typing import TextIO, TYPE_CHECKING
 
@@ -197,116 +198,35 @@ class BbpPairings(PairingEngine):
         return boards
 
 
-class BergerTables:
-    """Tables defining pairings in the Berger system
-    (FIDE handbook C.05.Annex 1: Details of Berger Table)"""
-
-    BERGER_TABLE_4_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 4), (2, 3)],
-        2: [(4, 3), (1, 2)],
-        3: [(2, 4), (3, 1)],
-    }
-
-    BERGER_TABLE_6_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 6), (2, 5), (3, 4)],
-        2: [(6, 4), (5, 3), (1, 2)],
-        3: [(2, 6), (3, 1), (4, 5)],
-        4: [(6, 5), (1, 4), (2, 3)],
-        5: [(3, 6), (4, 2), (5, 1)],
-    }
-
-    BERGER_TABLE_8_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 8), (2, 7), (3, 6), (4, 5)],
-        2: [(8, 5), (6, 4), (7, 3), (1, 2)],
-        3: [(2, 8), (3, 1), (4, 7), (5, 6)],
-        4: [(8, 6), (7, 5), (1, 4), (2, 3)],
-        5: [(3, 8), (4, 2), (5, 1), (6, 7)],
-        6: [(8, 7), (1, 6), (2, 5), (3, 4)],
-        7: [(4, 8), (5, 3), (6, 2), (7, 1)],
-    }
-
-    BERGER_TABLE_10_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 10), (2, 9), (3, 8), (4, 7), (5, 6)],
-        2: [(10, 6), (7, 5), (8, 4), (9, 3), (1, 2)],
-        3: [(2, 10), (3, 1), (4, 9), (5, 8), (6, 7)],
-        4: [(10, 7), (8, 6), (9, 5), (1, 4), (2, 3)],
-        5: [(3, 10), (4, 2), (5, 1), (6, 9), (7, 8)],
-        6: [(10, 8), (9, 7), (1, 6), (2, 5), (3, 4)],
-        7: [(4, 10), (5, 3), (6, 2), (7, 1), (8, 9)],
-        8: [(10, 9), (1, 8), (2, 7), (3, 6), (4, 5)],
-        9: [(5, 10), (6, 4), (7, 3), (8, 2), (9, 1)],
-    }
-
-    BERGER_TABLE_12_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 12), (2, 11), (3, 10), (4, 9), (5, 8), (6, 7)],
-        2: [(12, 7), (8, 6), (9, 5), (10, 4), (11, 3), (1, 2)],
-        3: [(2, 12), (3, 1), (4, 11), (5, 10), (6, 9), (7, 8)],
-        4: [(12, 8), (9, 7), (10, 6), (11, 5), (1, 4), (2, 3)],
-        5: [(3, 12), (4, 2), (5, 1), (6, 11), (7, 10), (8, 9)],
-        6: [(12, 9), (10, 8), (11, 7), (1, 6), (2, 5), (3, 4)],
-        7: [(4, 12), (5, 3), (6, 2), (7, 1), (8, 11), (9, 10)],
-        8: [(12, 10), (11, 9), (1, 8), (2, 7), (3, 6), (4, 5)],
-        9: [(5, 12), (6, 4), (7, 3), (8, 2), (9, 1), (10, 11)],
-        10: [(12, 11), (1, 10), (2, 9), (3, 8), (4, 7), (5, 6)],
-        11: [(6, 12), (7, 5), (8, 4), (9, 3), (10, 2), (11, 1)],
-    }
-
-    BERGER_TABLE_14_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 14), (2, 13), (3, 12), (4, 11), (5, 10), (6, 9), (7, 8)],
-        2: [(14, 8), (9, 7), (10, 6), (11, 5), (12, 4), (13, 3), (1, 2)],
-        3: [(2, 14), (3, 1), (4, 13), (5, 12), (6, 11), (7, 10), (8, 9)],
-        4: [(14, 9), (10, 8), (11, 7), (12, 6), (13, 5), (1, 4), (2, 3)],
-        5: [(3, 14), (4, 2), (5, 1), (6, 13), (7, 12), (8, 11), (9, 10)],
-        6: [(14, 10), (11, 9), (12, 8), (13, 7), (1, 6), (2, 5), (3, 4)],
-        7: [(4, 14), (5, 3), (6, 2), (7, 1), (8, 13), (9, 12), (10, 11)],
-        8: [(14, 11), (12, 10), (13, 9), (1, 8), (2, 7), (3, 6), (4, 5)],
-        9: [(5, 14), (6, 4), (7, 3), (8, 2), (9, 1), (10, 13), (11, 12)],
-        10: [(14, 12), (13, 11), (1, 10), (2, 9), (3, 8), (4, 7), (5, 6)],
-        11: [(6, 14), (7, 5), (8, 4), (9, 3), (10, 2), (11, 1), (12, 13)],
-        12: [(14, 13), (1, 12), (2, 11), (3, 10), (4, 9), (5, 8), (6, 7)],
-        13: [(7, 14), (8, 6), (9, 5), (10, 4), (11, 3), (12, 2), (13, 1)],
-    }
-
-    BERGER_TABLE_16_PLAYERS: dict[int, list[tuple[int, int]]] = {
-        1: [(1, 16), (2, 15), (3, 14), (4, 13), (5, 12), (6, 11), (7, 10), (8, 9)],
-        2: [(16, 9), (10, 8), (11, 7), (12, 6), (13, 5), (14, 4), (15, 3), (1, 2)],
-        3: [(2, 16), (3, 1), (4, 15), (5, 14), (6, 13), (7, 12), (8, 11), (9, 10)],
-        4: [(16, 10), (11, 9), (12, 8), (13, 7), (14, 6), (15, 5), (1, 4), (2, 3)],
-        5: [(3, 16), (4, 2), (5, 1), (6, 15), (7, 14), (8, 13), (9, 12), (10, 11)],
-        6: [(16, 11), (12, 10), (13, 9), (14, 8), (15, 7), (1, 6), (2, 5), (3, 4)],
-        7: [(4, 16), (5, 3), (6, 2), (7, 1), (8, 15), (9, 14), (10, 13), (11, 12)],
-        8: [(16, 12), (13, 11), (14, 10), (15, 9), (1, 8), (2, 7), (3, 6), (4, 5)],
-        9: [(5, 16), (6, 4), (7, 3), (8, 2), (9, 1), (10, 15), (11, 14), (12, 13)],
-        10: [(16, 13), (14, 12), (15, 11), (1, 10), (2, 9), (3, 8), (4, 7), (5, 6)],
-        11: [(6, 16), (7, 5), (8, 4), (9, 3), (10, 2), (11, 1), (12, 15), (13, 14)],
-        12: [(16, 14), (15, 13), (1, 12), (2, 11), (3, 10), (4, 9), (5, 8), (6, 7)],
-        13: [(7, 16), (8, 6), (9, 5), (10, 4), (11, 3), (12, 2), (13, 1), (14, 15)],
-        14: [(16, 15), (1, 14), (2, 13), (3, 12), (4, 11), (5, 10), (6, 9), (7, 8)],
-        15: [(8, 16), (9, 7), (10, 6), (11, 5), (12, 4), (13, 3), (14, 2), (15, 1)],
-    }
-
-    BERGER_TABLES: dict[int, dict[int, list[tuple[int, int]]]] = {
-        4: BERGER_TABLE_4_PLAYERS,
-        6: BERGER_TABLE_6_PLAYERS,
-        8: BERGER_TABLE_8_PLAYERS,
-        10: BERGER_TABLE_10_PLAYERS,
-        12: BERGER_TABLE_12_PLAYERS,
-        14: BERGER_TABLE_14_PLAYERS,
-        16: BERGER_TABLE_16_PLAYERS,
-    }
-
-    @classmethod
-    def table_from_player_count(
-        cls, player_count: int
-    ) -> dict[int, list[tuple[int, int]]]:
-        return cls.BERGER_TABLES[
-            player_count if player_count % 2 == 0 else player_count + 1
-        ]
-
-
 class BergerPairingEngine(PairingEngine):
     MIN_PLAYERS = 3
-    MAX_PLAYERS = 16
+
+    @staticmethod
+    @cache
+    def generate_berger_table(player_count: int) -> dict[int, list[tuple[int, int]]]:
+        if player_count <= 2:
+            raise ValueError(f'There must be at least 3 players, got {player_count}')
+        odd_players = player_count % 2 == 1
+        if odd_players:
+            player_count += 1
+        round_count = player_count - 1
+        previous_pairings = [
+            (i + 1, player_count - i) for i in range(player_count // 2)
+        ]
+        berger_table = {1: previous_pairings}
+        for round_ in range(2, round_count + 1):
+            pairings = previous_pairings[:]
+            if round_ % 2 == 1:
+                pairings[0] = previous_pairings[-1][1], previous_pairings[0][0]
+                pairings[-1] = previous_pairings[0][1], previous_pairings[1][0]
+            else:
+                pairings[0] = previous_pairings[0][1], previous_pairings[-1][1]
+                pairings[-1] = previous_pairings[0][0], previous_pairings[1][0]
+            for i in range(2, player_count // 2):
+                pairings[-i] = previous_pairings[i - 1][1], previous_pairings[i][0]
+            berger_table[round_] = pairings
+            previous_pairings = pairings
+        return berger_table
 
     def invalid_player_count_message(self, tournament: 'Tournament') -> str | None:
         player_count = tournament.player_count
@@ -314,10 +234,6 @@ class BergerPairingEngine(PairingEngine):
             return _(
                 'Too few players to generate the pairings (minimum: {min})'
             ).format(min=self.MIN_PLAYERS)
-        if player_count > self.MAX_PLAYERS:
-            return _(
-                'Too many players to generate the pairings (maximum: {max})'
-            ).format(max=self.MAX_PLAYERS)
         expected_rounds = player_count if player_count % 2 == 1 else player_count - 1
         if tournament.rounds != expected_rounds:
             return _(
@@ -337,9 +253,7 @@ class BergerPairingEngine(PairingEngine):
 
     def _generate_boards(self, tournament: 'Tournament', round_: int) -> list[Board]:
         boards: list[Board] = []
-        round_pairings = BergerTables.table_from_player_count(tournament.player_count)[
-            round_
-        ]
+        round_pairings = self.generate_berger_table(tournament.player_count)[round_]
         player_by_pairing_number = {
             pairing_number: tournament.players_by_id[player_id]
             for player_id, pairing_number in BergerNumbersSetting.get_value(
