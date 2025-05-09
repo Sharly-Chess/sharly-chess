23dffae28256f151ff732fddedb967992c5d094e
pascalaubry <pascalaubry35@gmail.com>
2025-05-09 08:38:33 +0200

    Fixed ChessEvent players' rating types

--- src/plugins/chessevent/data/chessevent_player.py
+++ src/plugins/chessevent/data/chessevent_player.py
@@ -1,75 +1,17 @@
-from enum import IntEnum
 from logging import Logger
 from typing import Any
 
-from common.i18n import _
 from common.logger import get_logger
 from utils.enum import (
     PlayerGender,
     PlayerCategory,
     PlayerRatingType,
+    PlayerTitle,
 )
 from plugins.chessevent.data.chessevent_field_reader import ChessEventFieldReader
 from plugins.ffe.utils import PlayerFFELicence
 
 logger: Logger = get_logger()
-
-
-class ChessEventPlayerTitle(IntEnum):
-    """The possible FIDE titles for ChessEvent players: GM, WGM, IM, WIM, FM, WFM.
-    Also includes the "no title" case, but does not include CM nor WCM.
-    This is for Papi-compatibility reasons."""
-
-    NONE = 0
-    WOMAN_FIDE_MASTER = 1
-    FIDE_MASTER = 2
-    WOMAN_INTERNATIONAL_MASTER = 3
-    INTERNATIONAL_MASTER = 4
-    WOMAN_GRANDMASTER = 5
-    GRANDMASTER = 6
-
-    @property
-    def to_papi_value(self) -> str:
-        match self:
-            case ChessEventPlayerTitle.NONE:
-                return ''
-            case ChessEventPlayerTitle.WOMAN_FIDE_MASTER:
-                return 'ff'
-            case ChessEventPlayerTitle.FIDE_MASTER:
-                return 'f'
-            case ChessEventPlayerTitle.WOMAN_INTERNATIONAL_MASTER:
-                return 'mf'
-            case ChessEventPlayerTitle.INTERNATIONAL_MASTER:
-                return 'm'
-            case ChessEventPlayerTitle.WOMAN_GRANDMASTER:
-                return 'gf'
-            case ChessEventPlayerTitle.GRANDMASTER:
-                return 'g'
-            case _:
-                raise ValueError(f'Unknown title: {self}')
-
-    @property
-    def short_name(self) -> str:
-        match self:
-            case ChessEventPlayerTitle.NONE:
-                return ''
-            case ChessEventPlayerTitle.WOMAN_FIDE_MASTER:
-                return _('WFM *** SHORT NAME FOR Woman Fide Master')
-            case ChessEventPlayerTitle.FIDE_MASTER:
-                return _('FM *** SHORT NAME FOR Fide Master')
-            case ChessEventPlayerTitle.WOMAN_INTERNATIONAL_MASTER:
-                return _('WIM *** SHORT NAME FOR Woman International Master')
-            case ChessEventPlayerTitle.INTERNATIONAL_MASTER:
-                return _('IM *** SHORT NAME FOR International Master')
-            case ChessEventPlayerTitle.WOMAN_GRANDMASTER:
-                return _('WGM *** SHORT NAME FOR Woman Grand Master')
-            case ChessEventPlayerTitle.GRANDMASTER:
-                return _('GM *** SHORT NAME FOR Grand Master')
-            case _:
-                raise ValueError(f'Unknown title: {self}')
-
-    def __str__(self) -> str:
-        return self.short_name
 
 
 class ChessEventPlayer:
@@ -86,6 +28,38 @@ class ChessEventPlayer:
                 return PlayerFFELicence.B
             case 3:
                 return PlayerFFELicence.A
+            case _:
+                raise ValueError(f'Unknown value: {value}')
+
+    @staticmethod
+    def rating_type_from_chessevent_value(value: int) -> PlayerRatingType:
+        match value:
+            case 1:
+                return PlayerRatingType.NATIONAL
+            case 2:
+                return PlayerRatingType.ESTIMATED
+            case 3:
+                return PlayerRatingType.FIDE
+            case _:
+                raise ValueError(f'Unknown value: {value}')
+
+    @staticmethod
+    def title_from_chessevent_value(value: int) -> PlayerTitle:
+        match value:
+            case 0:
+                return PlayerTitle.NONE
+            case 1:
+                return PlayerTitle.WOMAN_FIDE_MASTER
+            case 2:
+                return PlayerTitle.FIDE_MASTER
+            case 3:
+                return PlayerTitle.WOMAN_INTERNATIONAL_MASTER
+            case 4:
+                return PlayerTitle.INTERNATIONAL_MASTER
+            case 5:
+                return PlayerTitle.WOMAN_GRANDMASTER
+            case 6:
+                return PlayerTitle.GRANDMASTER
             case _:
                 raise ValueError(f'Unknown value: {value}')
 
@@ -117,20 +91,18 @@ class ChessEventPlayer:
                 'category', PlayerCategory, PlayerCategory.NONE
             )
             self.standard_rating = reader.get('standard_rating', int)
-            self.standard_rating_type = reader.get_enum(
-                'standard_rating_type', PlayerRatingType, PlayerRatingType.ESTIMATED
+            self.standard_rating_type = self.rating_type_from_chessevent_value(
+                reader.get('standard_rating_type', int)
             )
             self.rapid_rating = reader.get('rapid_rating', int)
-            self.rapide_rating_type = reader.get_enum(
-                'rapid_rating_type', PlayerRatingType, PlayerRatingType.ESTIMATED
+            self.rapide_rating_type = self.rating_type_from_chessevent_value(
+                reader.get('rapid_rating_type', int)
             )
             self.blitz_rating = reader.get('blitz_rating', int)
-            self.blitz_rating_type = reader.get_enum(
-                'blitz_rating_type', PlayerRatingType, PlayerRatingType.ESTIMATED
+            self.blitz_rating_type = self.rating_type_from_chessevent_value(
+                reader.get('blitz_rating_type', int)
             )
-            self.title = reader.get_enum(
-                'title', ChessEventPlayerTitle, ChessEventPlayerTitle.NONE
-            )
+            self.title = self.title_from_chessevent_value(reader.get('title', int))
 
             self.email = reader.get('email', str)
             self.phone = reader.get('phone', str)
