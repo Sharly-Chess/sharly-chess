67d26c4c2764c3cdd4f793c535c242b9c9115af3
Timothy Armes <tim@timothyarmes.com>
2025-05-10 14:48:03 +0200

    Prevent modal closing

--- src/web/controllers/admin/index_admin_controller.py
+++ src/web/controllers/admin/index_admin_controller.py
@@ -119,6 +119,7 @@ class IndexAdminController(BaseAdminController):
         request: HTMXRequest,
         admin_tab: str | None,
         modal: str | None = None,
+        keep_modal_open: bool | None = None,
         admin_events_show_details: bool | None = None,
         data: dict[str, str] | None = None,
         errors: dict[str, str] | None = None,
@@ -362,7 +363,9 @@ class IndexAdminController(BaseAdminController):
                 template_name='admin/modals.html',
                 context=context,
                 re_target='#modal-wrapper',
-                trigger_event='modal_opened',
+                trigger_event='modal_opened'
+                if not keep_modal_open
+                else 'static_modal_opened',
                 after='settle',
             )
         return HTMXTemplate(template_name='admin/index.html', context=context)
@@ -533,10 +536,12 @@ class IndexAdminController(BaseAdminController):
         self,
         request: HTMXRequest,
     ) -> Template | ClientRedirect:
+        papi_web_config: PapiWebConfig = PapiWebConfig()
         return self._admin_render(
             request,
             admin_tab='config',
             modal='config',
+            keep_modal_open=papi_web_config.force_edit,
         )
 
     @get(
