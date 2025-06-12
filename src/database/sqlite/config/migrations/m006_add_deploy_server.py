8091fce33c9642b15327b6962697565174d53234
Molrn <76754000+Molrn@users.noreply.github.com>
2025-06-12 19:46:30 +0200

    Prevent disabling server deployment for upgrading users

--- src/database/sqlite/config/migrations/m006_add_deploy_server.py
+++ src/database/sqlite/config/migrations/m006_add_deploy_server.py
@@ -3,9 +3,8 @@ from database.sqlite.migration import BaseMigration
 
 class Migration(BaseMigration):
     def forward(self):
-        self.database.execute(
-            'ALTER TABLE `info` ADD `deploy_server` INTEGER NOT NULL DEFAULT 0'
-        )
+        self.database.execute('ALTER TABLE `info` ADD `deploy_server` INTEGER')
+        self.database.execute('UPDATE `info` SET `deploy_server` = NOT `force_edit`')
 
     def backward(self):
         self.database.execute('ALTER TABLE `info` DROP COLUMN `deploy_server`')
