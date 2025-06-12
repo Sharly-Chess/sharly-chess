cad84341bde81b6e20d52dc04e73719f81130f47
Molrn <76754000+Molrn@users.noreply.github.com>
2025-06-12 10:57:15 +0200

    Fix mypy errors

--- src/plugins/chessevent/__init__.py
+++ src/plugins/chessevent/__init__.py
@@ -11,8 +11,9 @@ TMP_DIR: Path = common.TMP_DIR / PLUGIN_NAME
 try:
     TMP_DIR.mkdir(parents=True, exist_ok=True)
 except PermissionError as error:
-    from common.logger import logger
+    from common.logger import get_logger
 
+    logger = get_logger()
     logger.critical(
         'Could not create directory [%s]: %s',
         TMP_DIR.absolute(),
