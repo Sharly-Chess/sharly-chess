4205202e5ba1ce8cb957e6cc82f9be541a8577ec
Molrn <76754000+Molrn@users.noreply.github.com>
2025-07-04 18:16:40 +0200

    fix index delta for full pairings

--- src/data/pairings/engines.py
+++ src/data/pairings/engines.py
@@ -74,9 +74,12 @@ class PairingEngine(ABC):
             pab_board.stored_board.index += len(stored_boards)
         if self.reorder_boards:
             index_delta = max(
-                board.index
-                for board in tournament.get_round_boards(round_)
-                if board.black_player
+                (
+                    board.index
+                    for board in tournament.get_round_boards(round_)
+                    if board.black_player
+                )
+                or [0]
             )
             for index, board in enumerate(sorted(boards, reverse=True)):
                 board.stored_board.index = index_delta + index
