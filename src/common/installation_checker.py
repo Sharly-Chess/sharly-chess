35bd2bb4a0c6afb45e354836accd28316d406e3d
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2025-08-05 19:56:47 +0200

    Fixed unresolved reference

--- src/common/installation_checker.py
+++ src/common/installation_checker.py
@@ -115,7 +115,7 @@ class WebLibFileInstaller(WebLibInstaller):
 class InstallationChecker:
     """A class to check the installation of all the needed tools and libs."""
 
-    web_lib_installers: list[ToolInstaller] = [
+    web_lib_installers: list[WebLibInstaller] = [
         WebLibArchiveInstaller(
             'Bootstrap',
             SharlyChessConfig.bootstrap_version,
