9668fb7afab1caa865d5e0bdb53db42a432ace6f
Timothy Armes <tim@timothyarmes.com>
2025-08-05 11:39:45 +0200

    Update papi-converter

--- src/common/tool_installer.py
+++ src/common/tool_installer.py
@@ -326,7 +326,7 @@ class PapiConverterInstaller(ExecutableInstaller):
 
     @property
     def _version(self) -> Version:
-        return Version('1.0.0')
+        return Version('1.0.1')
 
     def install(self) -> bool:
         archive_filename = self.system_handler.archive_filename
