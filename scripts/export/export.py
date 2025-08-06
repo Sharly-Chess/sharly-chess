8c49247d4dda909bb42b980293fc3630c2b4578d
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2025-08-06 11:29:19 +0200

    Do not add bbpPairings license file to the EXE now

--- scripts/export/export.py
+++ scripts/export/export.py
@@ -150,9 +150,7 @@ def build_exe():
     custom_dir: Path = SOURCE_DIR / 'custom'
     files += [file for file in custom_dir.glob('**/*') if file.is_file()]
     files += [file for file in LOCALE_DIR.glob('**/*.mo') if file.is_file()]
-    files += [
-        file for file in BbpPairings().executable_dir.glob('**/*') if file.is_file()
-    ]
+    files += [BbpPairings().executable_path]
     files += [
         FFE_SQL_SERVER_CREDENTIALS_FILE,
     ]
