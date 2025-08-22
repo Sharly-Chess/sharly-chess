3c67a22838fe7e272ad18ec55df399c7cf4ca10e
Pascal AUBRY <66429491+pascalaubry@users.noreply.github.com>
2025-08-22 13:36:47 +0200

    improvement: Generate i18n.md only with i18n_update --generate-doc (#991)
    bugfix: Update the MO files when PO files only have changed

--- scripts/i18n/i18n_update.py
+++ scripts/i18n/i18n_update.py
@@ -1,3 +1,18 @@
+import argparse
+
 from common.i18n import update_i18n_files
 
-update_i18n_files()
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--generate-doc',
+        action='store_true',
+        help='Generate the documentation on i18n translation status',
+    )
+    args = parser.parse_args()
+    update_i18n_files(generate_doc=args.generate_doc)
+
+
+if __name__ == '__main__':
+    main()
