9809af150bd9b4e40c5b870860cfd6ad4b3d3c7a
Timothy Armes <tim@timothyarmes.com>
2025-08-26 19:58:04 +0200

    Oops

--- src/data/tournament.py
+++ src/data/tournament.py
@@ -128,25 +128,6 @@ class Tournament:
     def time_control_trf25(self) -> str:
         return self.stored_tournament.time_control_trf25 or ''
 
-    def _parse_time_control_trf25(self) -> tuple[int, int]:
-        """Parse initial time from TRF25 format. Returns (0, 0) for multi-period or asymmetric formats."""
-        trf25 = self.stored_tournament.time_control_trf25
-        if not trf25:
-            return 0, 0
-
-        try:
-            # Handle format: <secs> or <secs>+<inc>
-            if '+' in trf25:
-                parts = trf25.split('+')
-                if len(parts) == 2:
-                    return int(parts[0]), int(parts[1])
-                else:
-                    return 0, 0
-            else:
-                return int(trf25), 0
-        except (ValueError, AttributeError):
-            return 0, 0
-
     @property
     def time_control_initial_time(self) -> int:
         """Parse initial time from TRF25 format. Returns 0 for invalid formats."""
