d8375b586f81128420bd323c721355b5e6c15e92
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2025-08-29 14:16:34 +0200

    Finalized the test

--- src/common/engine.py
+++ src/common/engine.py
@@ -71,9 +71,8 @@ class Engine(ABC):
         if not InstallationChecker.check():
             self.error = True
             return
-        logger.error(f'{TEST_ENV=}')
         if TEST_ENV:
-            # skip all the upgrade stuff on TEST_ENV (recovering tests are specific)
+            # skip all the upgrade stuff on TEST_ENV (recovering tests run explicitely)
             return
         if more_recent_version and download_url:
             yes_answer = _('Y *** THE LETTER TO ANSWER YES')
--- tests/recover/base/test_recover.py
+++ tests/recover/base/test_recover.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 import pytest
 from packaging.version import Version
 
-from common import LOG_FILE
+from common import LOG_FILE, EVENTS_FOLDER
 from common.logger import get_logger
 from common.engine import Engine
 from data.event import Event
@@ -27,13 +27,17 @@ class RecoverTestCase(TestCase):
     """Tests the recovering of 2.4.0 events."""
 
     def test_recover(self):
+        # Note(pascalaubry): engines recover previous releases data
+        # with method _recover_previous_version() by looking
+        # for previous instances in folder ..
+        logger.info('Loading test engine...')
+        test_engine = _TestEngine()
         version: Version = Version('2.4.0')
         dir_name: str = f'papi-web-{version}'
         cwd: str = os.getcwd()
         os.chdir(Path(__file__).parent)
-        files: list[Path] = [file for file in (Path('../..') / dir_name).glob('*.db')]
-        logger.info('Loading test engine...')
-        test_engine = _TestEngine()
+        events_folder: Path = Path('..') / dir_name / EVENTS_FOLDER
+        files: list[Path] = [file for file in events_folder.glob('*.db')]
         logger.info('Recovering version [%s]...', version)
         test_engine._recover_previous_version(
             version,
@@ -45,6 +49,6 @@ class RecoverTestCase(TestCase):
             logger.info('Loading event [%s]]...', event_uniq_id)
             event: Event = EventLoader().load_event(event_uniq_id)
             logger.info('Tournaments: %d', len(event.tournaments_by_id))
-            logger.info('Players: %d', len(event.tournaments_by_id))
+            logger.info('Players: %d', len(event.players_by_id))
         logger.info('Done.')
         os.chdir(cwd)
