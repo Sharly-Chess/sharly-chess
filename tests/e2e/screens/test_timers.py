6b75df9f42dca98350e2f8a9d381180c63b618f4
Molrn <76754000+Molrn@users.noreply.github.com>
2025-09-01 20:12:49 +0200

    Fix timer creation (#1071)

---
+++ tests/e2e/screens/test_timers.py
@@ -0,0 +1,37 @@
+import pytest
+from playwright.sync_api import Page, expect, APIRequestContext
+from tests.test_config import TestUtils
+
+
+EVENT_ID = 'event-test-timer'
+
+
+@pytest.fixture(scope='module', autouse=True)
+def setup(api_request_context: APIRequestContext):
+    TestUtils.create_event(EVENT_ID, via_api_request_context=api_request_context)
+    yield
+    TestUtils.delete_event(EVENT_ID, via_api_request_context=api_request_context)
+
+
+@pytest.mark.e2e
+class TestTimersFunctionality:
+    def test_create_and_delete_timer(self, page: Page):
+        page.goto(f'/admin/event/{EVENT_ID}/timers')
+        TestUtils.button_by_text(page, 'Create a timer').click()
+        modal = page.locator('.modal-dialog')
+        expect(modal).to_be_visible()
+        name = 'Test Timer'
+        modal.get_by_role('textbox', name='Name:').fill(name)
+        modal.locator('button[type=submit]').click()
+        TestUtils.button_by_text(page, 'Cancel').click()
+        TestUtils.button_by_text(page, 'Close').click()
+
+        card = page.locator(f"div.card:has-text('{name}')")
+        expect(card).to_be_visible()
+
+        button = card.locator('button[hx-get*="delete"]')
+        button.click()
+        TestUtils.button_by_text(modal, 'Delete').click()
+
+        expect(page.get_by_text(f'Timer [{name}] has been deleted.')).to_be_visible()
+        expect(page.locator(f"div.card:has-text('{name}')")).not_to_be_attached()
