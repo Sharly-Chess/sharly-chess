04c302722ac66cf5f178ff607a2c121e75132b1f
Timothy Armes <tim@timothyarmes.com>
2025-09-10 11:22:59 +0200

    Move print handling

--- src/web/controllers/admin/event_print_controller.py
+++ src/web/controllers/admin/event_print_controller.py
@@ -6,17 +6,21 @@ from litestar.enums import RequestEncodingType
 from litestar.params import Body
 from litestar.response import Template, Redirect
 from litestar_htmx import ClientRedirect
+import urllib
 
 from common.exception import OptionError
 from common.i18n import _
+from data.player import plugin_manager
 from data.tournament import Tournament
 from data.print_documents import (
     PrintDocument,
     PrintDocumentManager,
     PrintDocumentOptionManager,
+    PrintOption,
 )
 from data.print_documents.documents import PlayerListPrintDocument
 from data.print_documents.options import TournamentPrintOption
+from plugins.hookspec import ExtraColumn
 from web.controllers.base_controller import WebContext
 from web.controllers.admin.base_event_admin_controller import (
     BaseEventAdminController,
@@ -63,6 +67,7 @@ class EventPrintController(BaseEventAdminController):
                 }
                 | {
                     'tournament': WebContext.value_to_form_data(tournament_id),
+                    'tournaments': WebContext.value_to_form_data(tournament_id),
                 }
             )
         containers_by_document: dict[str, list[str]] = {'': []} | {
@@ -200,4 +205,62 @@ class EventPrintController(BaseEventAdminController):
         return self._admin_print_render(
             web_context=web_context,
             template_context=template_context,
+        )
+
+    @get(
+        path='/admin/print-view/{event_uniq_id:str}/{document: str}',
+        name='admin-print-view',
+    )
+    async def htmx_tournament_print_view(
+        self,
+        request: HTMXRequest,
+        event_uniq_id: str,
+        document: str,
+        options: str | None = None,
+    ) -> Template | ClientRedirect | Redirect:
+        web_context: BaseEventAdminWebContext = BaseEventAdminWebContext(
+            request,
+            event_uniq_id=event_uniq_id,
+            data=None,
+        )
+        if web_context.error:
+            return web_context.error
+        document_type = PrintDocumentManager.get_type(document)
+        option_data: dict[str, str] = {}
+        if options:
+            for option in urllib.parse.unquote(options).split('|'):
+                key, raw_value = option.split('=')
+                option_data[key] = raw_value
+        print_options: list[PrintOption] = []
+        for print_option in document_type.default_options():
+            value = WebContext.form_data_to_value(
+                option_data, print_option.id, print_option.type
+            )
+            print_options.append(type(print_option)(value))
+        print_document = document_type(web_context.get_admin_event(), print_options)
+
+        per_plugin_columns = plugin_manager.hook.get_extra_print_view_columns(
+            document=print_document
+        )
+        extra_columns: dict[str, list[ExtraColumn]] = {}
+        for plugin_columns in per_plugin_columns:
+            for extra_column in plugin_columns:
+                c = extra_columns.setdefault(extra_column.at, [])
+                c.append(extra_column)
+        per_plugin_css: list[str] = plugin_manager.hook.get_extra_print_view_css(
+            document=print_document
+        )
+        extra_css: str = '\n'.join(per_plugin_css)
+
+        template_context = (
+            web_context.template_context
+            | {
+                'document': print_document,
+                'extra_columns': extra_columns,
+                'extra_css': extra_css,
+            }
+            | print_document.template_context
+        )
+        return HTMXTemplate(
+            template_name=print_document.template_name, context=template_context
         )
