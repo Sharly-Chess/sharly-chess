f42f083bfcef755fb8f2ad3164a02484e06ca5f8
Sammy Plat <sammy@sammyplat.fr>
2025-09-11 13:00:11 +0200

    Add comments and uniformize behaviour

--- src/database/sqlite/event/views/v001_all_boards.sql
+++ src/database/sqlite/event/views/v001_all_boards.sql
@@ -1,30 +1,45 @@
 DROP VIEW IF EXISTS "all_boards";
 CREATE VIEW "all_boards" AS
+-- BOARD DATA
 SELECT `p`.`tournament_id`, `p`.`round`,
-COALESCE(LIKELY(NULLIF(`white_player`.`fixed`, 0)), LIKELY(NULLIF(`black_player`.`fixed`, 0)), `board`.`index` + 1) as `board_number`,
+COALESCE(
+    -- LIKELY is only an optimization hint, it does not affect the result of the computation.
+    LIKELY(NULLIF(`white_player`.`fixed`, 0)),
+    LIKELY(NULLIF(`black_player`.`fixed`, 0)),
+    `board`.`index` + 1
+) AS `board_number`,
 `p`.`board_id`,
-`white_player`.`id` as `white_id`,
-`white_player`.`fide_id` as `white_fide_id`,
-`white_player`.`title` as `white_title`,
-`white_player`.`gender` as `white_gender`,
+
+-- WHITE PLAYER'S DATA
+`white_player`.`id` AS `white_id`,
+`white_player`.`fide_id` AS `white_fide_id`,
+`white_player`.`title` AS `white_title`,
+`white_player`.`gender` AS `white_gender`,
 CASE
+    -- Only show the name in format "LAST_NAME, First_Name" if they have a non-empty first name
 	WHEN `white_player`.`first_name` IS NULL or `white_player`.`first_name` = '' THEN
 		`white_player`.`last_name`
 	ELSE
 		CONCAT(`white_player`.`last_name`, ', ', `white_player`.`first_name`)
-END as `white_name`,
-`white_player`.`federation` as `white_federation`,
+END AS `white_name`,
+`white_player`.`federation` AS `white_federation`,
 CASE
+    -- when we are in a rapid or Blitz tournament (TournamentRating.STANDARD == 1 in the Pytho code)
 	WHEN `tournament`.`rating` <> 1
+        -- and the player's rating is not FIDE (RatingType.FIDE == 3 in the Python code)
 		AND json_extract(`white_player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
+        -- and we override non-FIDE ratings (either ON tournament or using the default event-level option)
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
+        -- but only if the player is not estimated in standard rating (RatingType.ESTIMATED == 1)
 		AND json_extract(`white_player`.`ratings`, '$.`1`.`type`') <> 1
 		THEN
+            -- then we override the rating with the player's standard rating
 			json_extract(`white_player`.`ratings`, '$.`1`.`value`')
 	ELSE
 		json_extract(`white_player`.`ratings`, '$.' || `tournament`.`rating` || '.value')
-END as `white_rating`,
+END AS `white_rating`,
 CASE
+    -- see above, same reasoning
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`white_player`.`ratings`,  '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
@@ -33,31 +48,39 @@ CASE
 		json_extract(`white_player`.`ratings`, '$.`1`.`type`')
 	ELSE
 		json_extract(`white_player`.`ratings`, '$.' || `tournament`.`rating` || '.type')
-END as `white_rating_type`,
+END AS `white_rating_type`,
 CASE
+    -- This an easier way to know if the player's rating was overridden, if this information needs displaying
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`white_player`.`ratings`,  '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
 		AND json_extract(`white_player`.`ratings`, '$.`1`.`type`') <> 1
 	THEN 1
     ELSE 0
-END as `white_overridden`,
-`white_player`.`plugin_data` as `white_plugin_data`,
-`p`.`result` as `white_result`,
-`o`.`result` as `black_result`,
-`black_player`.`id` as `black_id`,
-`black_player`.`fide_id` as `black_fide_id`,
-`black_player`.`title` as `black_title`,
-`black_player`.`gender` as `black_gender`,
+END AS `white_overridden`,
+`white_player`.`plugin_data` AS `white_plugin_data`,
+
+-- RESULTS
+`p`.`result` AS `white_result`,
+-- Note that black's result can be different from the opposite of white's result.
+-- This is the case when one player gets a score penalty.
+`o`.`result` AS `black_result`,
+
+-- BLACK PLAYER'S DATA
+`black_player`.`id` AS `black_id`,
+`black_player`.`fide_id` AS `black_fide_id`,
+`black_player`.`title` AS `black_title`,
+`black_player`.`gender` AS `black_gender`,
 CASE
-	WHEN `black_player`.`first_name` is NULL OR `black_player`.`first_name` = '' THEN
+-- See the white player's name for information
+	WHEN `black_player`.`first_name` IS NULL OR `black_player`.`first_name` = '' THEN
 		`black_player`.`last_name`
 	ELSE
 		CONCAT(`black_player`.`last_name`, ', ', `black_player`.`first_name`)
-END as `black_name`,
-`black_player`.`federation` as `black_federation`,
-
+END AS `black_name`,
+`black_player`.`federation` AS `black_federation`,
 CASE
+-- See the white player's rating for the reasoning
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`black_player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
@@ -66,8 +89,9 @@ CASE
 			json_extract(`black_player`.`ratings`, '$.`1`.`value`')
 	ELSE
 		json_extract(`black_player`.`ratings`, '$.' || `tournament`.`rating` || '.value')
-END as `black_rating`,
+END AS `black_rating`,
 CASE
+-- same as white player's rating type
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`black_player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
@@ -76,7 +100,7 @@ CASE
 		json_extract(`black_player`.`ratings`, '$.`1`.`type`')
 	ELSE
 		json_extract(`black_player`.`ratings`, '$.' || `tournament`.`rating` || '.type')
-END as `black_rating_type`,
+END AS `black_rating_type`,
 CASE
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`black_player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
@@ -84,12 +108,15 @@ CASE
 		AND json_extract(`black_player`.`ratings`, '$.`1`.`type`') <> 1
     THEN 1
     ELSE 0
-END as `black_overridden`,
-`black_player`.`plugin_data` as `black_plugin_data`
-FROM `pairing` as `p`
+END AS `black_overridden`,
+`black_player`.`plugin_data` AS `black_plugin_data`
+FROM `pairing` AS `p`
+-- NOTE(Amaras): I have made the choice to only show one line per pairing, so the white player is "prioritized"
 LEFT JOIN `board` ON (`p`.`board_id` = `board`.`id` AND `p`.`player_id` = `board`.`white_player_id`)
-LEFT JOIN `pairing` as `o` ON (`o`.`tournament_id` = `p`.`tournament_id` AND `o`.`round` = `p`.`round` AND `o`.`player_id` = `board`.`black_player_id`)
-LEFT JOIN `player` as `white_player` ON `white_player`.`id` = `board`.`white_player_id`
-LEFT JOIN `player` as `black_player` ON `black_player`.`id` = `board`.`black_player_id`
+-- o for opponent: the black player's pairing
+LEFT JOIN `pairing` AS `o` ON (`o`.`tournament_id` = `p`.`tournament_id` AND `o`.`round` = `p`.`round` AND `o`.`player_id` = `board`.`black_player_id`)
+LEFT JOIN `player` AS `white_player` ON `white_player`.`id` = `board`.`white_player_id`
+LEFT JOIN `player` AS `black_player` ON `black_player`.`id` = `board`.`black_player_id`
 LEFT JOIN `tournament` ON `tournament`.`id` = `p`.`tournament_id`
+-- There is only one line per event
 JOIN `info` ON `info`.`ROWID` = 1;
--- src/database/sqlite/event/views/v002_all_byes.sql
+++ src/database/sqlite/event/views/v002_all_byes.sql
@@ -1,25 +1,36 @@
 DROP VIEW IF EXISTS "all_byes";
 CREATE VIEW "all_byes" AS
-SELECT `pairing`.`tournament_id`, `pairing`.`round`,
-`player`.`id` as `player_id`, `player`.`fide_id`, `player`.`title`,
+SELECT
+-- PAIRING INFORMATION
+`pairing`.`tournament_id`, `pairing`.`round`,
+
+-- PLAYER INFORMATION
+`player`.`id` AS `player_id`, `player`.`fide_id`, `player`.`title`,
 CASE
+    -- Only show the name in "LAST_NAME, First_Name" format if the first name is not empty
     WHEN `player`.`first_name` IS NULL OR `player`.`first_name` = '' THEN
         `player`.`last_name`
     ELSE
         `player`.`last_name` || ', ' || `player`.`first_name`
-END as `name`,
+END AS `name`,
 `player`.`federation`,
 CASE
+    -- when we are in a rapid or Blitz tournament (TournamentRating.STANDARD == 1 in the Pytho code)
 	WHEN `tournament`.`rating` <> 1
+        -- and the player's rating is not FIDE (RatingType.FIDE == 3 in the Python code)
 		AND json_extract(`player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
+        -- and we override non-FIDE ratings (either ON tournament or using the default event-level option)
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
+        -- but only if the player is not estimated in standard rating (RatingType.ESTIMATED == 1)
 		AND json_extract(`player`.`ratings`, '$.`1`.`type`') <> 1
 		THEN
+            -- then we override the rating with the player's standard rating
 			json_extract(`player`.`ratings`, '$.`1`.`value`')
 	ELSE
 		json_extract(`player`.`ratings`, '$.' || `tournament`.`rating` || '.value')
-END as `rating`,
+END AS `rating`,
 CASE
+    -- see above, same reasoning
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
@@ -28,19 +39,23 @@ CASE
 		json_extract(`player`.`ratings`, '$.`1`.`type`')
 	ELSE
 		json_extract(`player`.`ratings`, '$.' || `tournament`.`rating` || '.type')
-END as `rating_type`,
+END AS `rating_type`,
 CASE
+    -- This an easier way to know if the player's rating was overridden, if this information needs displaying
 	WHEN `tournament`.`rating` <> 1
 		AND json_extract(`player`.`ratings`, '$.' || `tournament`.`rating` || '.type') <> 3
 		AND coalesce(`tournament`.`override_unrated_rapid_blitz`, `info`.`override_unrated_rapid_blitz`) = 1
 		AND json_extract(`player`.`ratings`, '$.`1`.`type`') <> 1
 	THEN 1
     ELSE 0
-END as `overriden`,
+END AS `overriden`,
 `player`.`plugin_data`,
-`pairing`.`result` as `result`
+`pairing`.`result` AS `result`
 FROM `pairing`
-LEFT JOIN `player` on `player`.`id` = `pairing`.`player_id`
+LEFT JOIN `player` ON `player`.`id` = `pairing`.`player_id`
 LEFT JOIN `tournament` ON `tournament`.`id` = `pairing`.`tournament_id`
-JOIN `info` on `info`.`ROWID` = 1
+-- There is only one info row per event.
+JOIN `info` ON `info`.`ROWID` = 1
+-- Only pairings without a board are games not scheduled.
+-- However, this also includes the Pairing Allocated Bye so this is a special case
 WHERE `pairing`.`board_id` IS NULL;
