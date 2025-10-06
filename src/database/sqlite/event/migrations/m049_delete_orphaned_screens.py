645779b51261f81611657c6302654c5248c693e7
Timothy Armes <tim@timothyarmes.com>
2025-10-06 16:50:29 +0200

    Delete orphaned screens (#1298)

---
+++ src/database/sqlite/event/migrations/m049_delete_orphaned_screens.py
@@ -0,0 +1,22 @@
+from database.sqlite.migration import BaseMigration
+
+
+class Migration(BaseMigration):
+    def forward(self):
+        # A bug that has been corrected in version 3.1.4 led to a situation where databases older than migration 10 would lose
+        # all the screenset data when updated by a version of the app > 2.8 (when PRAGMA foreign_keys was turned on).
+        # This leaves the database in a state where it's impossible to access the screens page.
+        # We can do no better than deleting these screens to allow the application to work.
+        self.database.execute(
+            """
+            DELETE FROM screen
+            WHERE id NOT IN (
+                SELECT DISTINCT screen_id FROM screen_set
+            ) 
+            AND 
+            type IN ('input', 'boards', 'players')
+            """
+        )
+
+    def backward(self):
+        pass
