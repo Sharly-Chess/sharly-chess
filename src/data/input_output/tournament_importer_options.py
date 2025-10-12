6e89026d2930b25cd3463f79f62e177f538eaa33
Molrn <76754000+Molrn@users.noreply.github.com>
2025-10-12 16:34:36 +0200

    Specify rating type on tournament TRF import

--- src/data/input_output/tournament_importer_options.py
+++ src/data/input_output/tournament_importer_options.py
@@ -7,6 +7,7 @@ from typing import Any
 from common.exception import OptionError
 from common.i18n import _
 from data.tournament import Tournament
+from utils.enum import TournamentRating
 from utils.option import Option
 
 
@@ -30,6 +31,11 @@ class TournamentImporterOption(Option, ABC):
     def get_default_value(self, tournament: Tournament | None = None) -> Any:
         """Set the default form value from the event or tournament on the modal."""
 
+    @property
+    def template_context(self) -> dict[str, Any]:
+        """Context to add to the template for the option."""
+        return {}
+
 
 class FileOption(TournamentImporterOption):
     @staticmethod
@@ -47,3 +53,33 @@ class FileOption(TournamentImporterOption):
         super().validate()
         if self.value is None:
             raise OptionError(_('A file is expected.'), self)
+
+
+class TournamentRatingOption(TournamentImporterOption):
+    @staticmethod
+    def static_id() -> str:
+        return 'tournament_rating'
+
+    @property
+    def type(self) -> type | UnionType:
+        return int
+
+    def get_default_value(self, tournament: Tournament | None = None) -> Any:
+        if tournament:
+            return tournament.rating.value
+        return TournamentRating.STANDARD.value
+
+    def validate(self):
+        super().validate()
+        try:
+            TournamentRating(self.value)
+        except ValueError:
+            raise OptionError(f'Unknown tournament type {self.value}', self)
+
+    @property
+    def template_context(self) -> dict[str, Any]:
+        return {
+            'rating_options': {
+                str(rating.value): rating.short_name for rating in TournamentRating
+            }
+        }
