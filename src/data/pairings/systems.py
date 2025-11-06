2f339cb472195e8fffd32e513b0472f7e56f3974
Molrn <76754000+Molrn@users.noreply.github.com>
2025-11-06 11:49:14 +0100

    Prevent mid-tournament check-ins for Round-Robin (#1432)
    * Restrict check-in to before the tournament start for Round-Robin
    
    * Review

--- src/data/pairings/systems.py
+++ src/data/pairings/systems.py
@@ -88,6 +88,11 @@ class PairingSystem[PV: PairingVariation](IdentifiableEntity, ABC):
         return False
 
     @property
+    def allow_mid_tournament_check_in(self) -> bool:
+        """Defines if check-ins are allowed after the start of the tournament."""
+        return True
+
+    @property
     def variation_field_id(self) -> str:
         """ID of the form field selecting the variation of the system."""
         return f'{self.id}_pairing_variation'
@@ -250,6 +255,10 @@ class RoundRobinPairingSystem(PairingSystem['RoundRobinVariation']):
     @property
     def hide_pab_value_field(self) -> bool:
         return True
+
+    @property
+    def allow_mid_tournament_check_in(self) -> bool:
+        return False
 
     @property
     def pairing_buttons_template(self) -> str:
