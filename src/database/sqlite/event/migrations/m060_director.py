0f6cc29110e73ce161940252a41334a14371ee24
Molrn <76754000+Molrn@users.noreply.github.com>
2025-11-19 10:08:40 +0100

    Knitpick

--- src/database/sqlite/event/migrations/m060_director.py
+++ src/database/sqlite/event/migrations/m060_director.py
@@ -3,7 +3,7 @@ from database.sqlite.migration import BaseMigration
 
 class Migration(BaseMigration):
     def forward(self):
-        self.database.execute('ALTER TABLE `info` ADD `organiser_director` INTEGER')
+        self.database.execute('ALTER TABLE `info` ADD `organiser_director` TEXT')
 
     def backward(self):
         self.database.execute('ALTER TABLE `info` DROP COLUMN `organiser_director`')
