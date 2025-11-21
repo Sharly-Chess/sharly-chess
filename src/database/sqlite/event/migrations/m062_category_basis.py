79f3e536a53a6b3b90a86c1201dd036612f14902
Molrn <76754000+Molrn@users.noreply.github.com>
2025-11-21 09:10:59 +0100

    Fix db column types

--- src/database/sqlite/event/migrations/m062_category_basis.py
+++ src/database/sqlite/event/migrations/m062_category_basis.py
@@ -3,7 +3,7 @@ from database.sqlite.migration import BaseMigration
 
 class Migration(BaseMigration):
     def forward(self):
-        self.database.execute('ALTER TABLE `info` ADD `age_category_base_date` FLOAT')
+        self.database.execute('ALTER TABLE `info` ADD `age_category_base_date` TEXT')
 
     def backward(self):
         self.database.execute('ALTER TABLE `info` DROP COLUMN `age_category_base_date`')
