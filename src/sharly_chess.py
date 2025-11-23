d3958c7c14531447913a9cdda39b07c2a778c001
Pascal AUBRY <66429491+pascalaubry@users.noreply.github.com>
2025-11-23 11:53:15 +0100

    Control the files installed on first startup (#1500)
    * Control the files installed on first startup
    
    * Check the installation ASAP (just after having unzipped)

--- src/sharly_chess.py
+++ src/sharly_chess.py
@@ -1,8 +1,56 @@
 import os
-import warnings
-from pathvalidate import validate_filepath, ValidationError
+from pathlib import Path
 import platform
 import sys
+
+if (
+    platform.system() == 'Windows'
+    and getattr(sys, 'frozen', False)
+    and os.getenv('TEST_ENV') != 'true'
+    and Path(sys.argv[0]).stem != 'pytest'
+):
+    # Microsoft Defender sometimes sends files to quarantaine when unzipping downloaded archives.
+    control_file: Path = Path('tmp', 'control_file.json')
+    if control_file.is_file():
+        import json
+        from typing import Any
+
+        with open(control_file, 'r', encoding='utf8') as infile:
+            control_data: dict[str, Any] = json.loads(infile.read())
+        version: list[str] = control_data['version']
+        file_paths: list[str] = control_data['file_paths']
+        missing_files: list[str] = [
+            file_path for file_path in file_paths if not Path(file_path).is_file()
+        ]
+        if missing_files:
+            import sys
+
+            message: str = '\n'.join(
+                [
+                    'Sharly Chess can not start because the following files are missing:',
+                ]
+                + [f'- {missing_file}' for missing_file in missing_files]
+                + [
+                    'This is probably due to Windows Defender or any other antivirus sending files to quarantaine.',
+                    'Recover the missing files from your quarantaine folder (depends on the antivirus you use) or manually install:',
+                    f'1. Download Sharly Chess from https://github.com/Sharly-Chess/sharly-chess/releases/download/{version}/sharly-chess-{version}-windows.zip',
+                    '2. Unzip the downloaded archive manually',
+                ]
+            )
+            import tkinter
+            from tkinter import messagebox
+
+            root = tkinter.Tk()
+            root.withdraw()
+            messagebox.showerror('Sharly Chess startup error', message)
+            root.destroy()
+            sys.exit(1)
+        # Remove the control file not to check twice when no missing file the first time.
+        control_file.unlink()
+
+import os
+import warnings
+from pathvalidate import validate_filepath, ValidationError
 
 # Nuclear option: Override warnings.warn to block specific messages
 # warnings.filterwarnings simply would not work
@@ -238,7 +286,7 @@ except Exception:
 
             root = tkinter.Tk()
             root.withdraw()
-            messagebox.showerror('Sharly Chess startup error', message)
+            messagebox.showerror(title, message)
             root.destroy()
 
         case 'Darwin':
