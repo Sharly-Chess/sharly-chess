6dd8a903a18f45b4b0a8b9700cf48d690d45a2fa
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2025-11-25 19:49:39 +0100

    Added sharly_chess_uac

--- src/antivirus.py
+++ src/antivirus.py
@@ -3,8 +3,6 @@ import platform
 import sys
 from abc import ABC
 from pathlib import Path
-from typing import Literal, Any
-from winreg import HKEYType
 
 
 def control_files() -> None:
@@ -76,7 +74,6 @@ if (
     and os.getenv('TEST_ENV') != 'true'
     and Path(sys.argv[0]).stem != 'pytest'
 ):
-    import winreg
 
     class WindowsAntivirus(Antivirus):
         def __init__(
@@ -86,137 +83,6 @@ if (
         ):
             super().__init__(name, signatures)
 
-        @classmethod
-        def _set_registry_value(
-            cls,
-            key: HKEYType | int,
-            sub_key: str,
-            name: str,
-            type: Literal[4, 5] | int,
-            value: Any,
-        ) -> bool:
-            """Sets the value of an entry of the registry, returns True on success, False on failure"""
-            try:
-                winreg.CreateKey(key, sub_key)
-                registry_key = winreg.OpenKey(
-                    key,
-                    sub_key,
-                    0,
-                    winreg.KEY_WRITE,
-                )
-                winreg.SetValueEx(
-                    registry_key,
-                    name,
-                    0,
-                    type,
-                    value,
-                )
-                winreg.CloseKey(registry_key)
-                return True
-            except WindowsError as we:
-                print(
-                    f'{cls.__name__}.set_registry_value(key=[{key}], sub_key=[{sub_key}], name=[{name}], value=[{value}]) raised an error: {we}'
-                )
-                return False
-
-        @classmethod
-        def _set_registry_hklm_value(
-            cls,
-            sub_key: str,
-            name: str,
-            type: Literal[4, 5] | int,
-            value: Any,
-        ) -> bool:
-            return cls._set_registry_value(
-                winreg.HKEY_LOCAL_MACHINE, sub_key, name, type, value
-            )
-
-        @classmethod
-        def set_registry_hklm_dword(
-            cls,
-            sub_key: str,
-            name: str,
-            value: Any,
-        ) -> bool:
-            return cls._set_registry_hklm_value(sub_key, name, winreg.REG_DWORD, value)
-
-        @classmethod
-        def _get_registry_value(
-            cls,
-            key: HKEYType | int,
-            sub_key: str,
-            name: str,
-        ) -> Any:
-            try:
-                registry_key = winreg.OpenKey(
-                    key,
-                    sub_key,
-                    0,
-                    winreg.KEY_READ,
-                )
-                value, regtype = winreg.QueryValueEx(registry_key, name)
-                winreg.CloseKey(registry_key)
-                return value
-            except WindowsError as we:
-                print(
-                    f'{cls.__name__}.get_registry_value(key=[{key}], sub_key=[{sub_key}], name=[{name}]) raised an error: {we}'
-                )
-                return None
-
-        @classmethod
-        def _get_registry_hklm_value(
-            cls,
-            sub_key: str,
-            name: str,
-        ) -> Any:
-            return cls._get_registry_value(winreg.HKEY_LOCAL_MACHINE, sub_key, name)
-
-        @classmethod
-        def _get_registry_hkcu_value(
-            cls,
-            sub_key: str,
-            name: str,
-        ) -> Any:
-            return cls._get_registry_value(winreg.HKEY_CURRENT_USER, sub_key, name)
-
-        @classmethod
-        def _get_registry_values(
-            cls,
-            key: HKEYType | int,
-            sub_key: str,
-        ) -> dict[str, Any]:
-            key_dict = {}
-            i = 0
-            try:
-                registry_key = winreg.OpenKey(key, sub_key, 0, winreg.KEY_READ)
-                while True:
-                    try:
-                        subvalue = winreg.EnumValue(registry_key, i)
-                    except WindowsError:
-                        break
-                    key_dict[subvalue[0]] = subvalue[1:]
-                    i += 1
-                return key_dict
-            except WindowsError as we:
-                print(
-                    f'{cls.__name__}.get_registry_values(key=[{key}], sub_key=[{sub_key}]) raised an error: {we}'
-                )
-                return key_dict
-
-        @classmethod
-        def _get_registry_hklm_values(
-            cls,
-            sub_key: str,
-        ) -> dict[str, Any]:
-            return cls._get_registry_values(winreg.HKEY_LOCAL_MACHINE, sub_key)
-
-        @classmethod
-        def _get_registry_hkcu_values(
-            cls,
-            sub_key: str,
-        ) -> dict[str, Any]:
-            return cls._get_registry_values(winreg.HKEY_CURRENT_USER, sub_key)
-
     class WindowsDefender(WindowsAntivirus):
         def __init__(self):
             super().__init__(
@@ -225,16 +91,6 @@ if (
                     'msmpeng.exe',
                     'mpcmdrun.exe',
                 ],
-            )
-
-        @classmethod
-        def get_exceptions(
-            cls,
-        ) -> list[str]:
-            return list(
-                cls._get_registry_hklm_values(
-                    r'SOFTWARE\Microsoft\Windows Defender\Exclusions\Paths'
-                ).keys()
             )
 
         @staticmethod
