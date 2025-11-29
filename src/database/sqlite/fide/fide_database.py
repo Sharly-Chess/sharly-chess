44f86d55a478f681f132bccc1a7569a8096a4b3a
Molrn <76754000+Molrn@users.noreply.github.com>
2025-11-29 23:24:07 +0100

    Increase download requests timeout (#1527)
    + CR REP tie-break mapping

--- src/database/sqlite/fide/fide_database.py
+++ src/database/sqlite/fide/fide_database.py
@@ -84,7 +84,7 @@ class FideDatabase(LocalSourcePlayerDatabase):
             local_zip_file: Path = tmp_dir / os.path.basename(fide_database_url)
             try:
                 response: Response = get(
-                    fide_database_url, allow_redirects=True, timeout=5
+                    fide_database_url, allow_redirects=True, timeout=10
                 )
                 if response.status_code != 200:
                     logger.error(
