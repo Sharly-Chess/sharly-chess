58593a990b353e1f4074d1a7a142e1e86c6682be
Molrn <76754000+Molrn@users.noreply.github.com>
2025-12-17 17:25:56 +0100

    potato

--- src/plugins/ffe/migrations/m004_set_age_category_chage_month.py
+++ src/plugins/ffe/migrations/m004_set_age_category_chage_month.py
@@ -7,9 +7,7 @@ class Migration(BasePluginMigration):
         self.database.execute('SELECT `enabled_plugins` FROM `info`')
         enabled_plugins = json.loads(self.database.fetchone()['enabled_plugins'])
         if 'ffe' in enabled_plugins:
-            self.database.execute(
-                'UPDATE `info` SET `age_category_change_month` = ?', (9,)
-            )
+            self.database.execute('UPDATE `info` SET `age_category_change_month` = 9')
 
     def backward(self):
         pass
