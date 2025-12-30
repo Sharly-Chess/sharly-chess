31371db481302dddc7916b9cb06bc46bb339b947
Molrn <76754000+Molrn@users.noreply.github.com>
2025-12-30 20:25:46 +0100

    Apply tournament permissions to players and tournaments tabs (#1630)
    * Apply tournament permissions to players and tournaments tabs
    
    * RN
    
    * Fix tournament deletion
    
    * Fix player creation
    
    * rn

--- src/web/guards.py
+++ src/web/guards.py
@@ -48,9 +48,12 @@ class BaseGuard(ABC):
 
     @staticmethod
     def _authorize_tournament_action(
-        action: AuthAction, client: Client, request: HTMXRequest
+        action: AuthAction,
+        client: Client,
+        request: HTMXRequest,
+        search_form: bool = False,
     ):
-        tournament = RequestUtils.get_tournament(request)
+        tournament = RequestUtils.get_tournament(request, search_form)
         if not client.action_allowed_for_tournament(action, tournament.id):
             raise PermissionDeniedException(
                 f'Client [{client.account.full_name}] is not allowed '
@@ -74,11 +77,30 @@ class TournamentActionGuard(ActionGuard):
     Falls back to an ActionGuard if no tournament is provided.
     optional: event_uniq_id, tournament_id."""
 
+    def __init__(self, action: AuthAction, search_form: bool = False):
+        super().__init__(action)
+        self.search_form = search_form
+
     def authorize_client(self, client: Client, request: HTMXRequest):
-        if RequestUtils.get_optional_tournament(request):
+        if RequestUtils.get_optional_tournament(request, self.search_form):
             self._authorize_tournament_action(self.action, client, request)
         else:
             self._authorize_action(self.action, client)
+
+
+class PlayerTournamentActionGuard(ActionGuard):
+    """Guard validating if an action is allowed for the client on
+    one of the tournaments of the player.
+    Required: event_uniq_id, player_id."""
+
+    def authorize_client(self, client: Client, request: HTMXRequest):
+        player = RequestUtils.get_player(request)
+        if not client.action_allowed_for_player(self.action, player):
+            raise PermissionDeniedException(
+                f'Client [{client.account.full_name}] is not allowed '
+                f'to perform the action [{self.action.localized_name("en")}] '
+                f'on player [{player.full_name}].'
+            )
 
 
 class EventGuard(BaseGuard):
