d80056e0e82b6515dcaa77064d388ae69151764e
Pascal AUBRY <66429491+pascalaubry@users.noreply.github.com>
2025-12-31 04:49:30 +0100

    Fixed the translation script (#1626)
    * Fixed the translation script
    
    * Keep translation errors
    
    * Added option --locale
    Added methods run() and init_tools()
    
    * Improve token handling
    
    * Updated i18n.md
    
    * Updated i18n.md
    
    * Fixed PO files URLs
    
    * Fixed blank lines

--- src/common/i18n/babel_updaters.py
+++ src/common/i18n/babel_updaters.py
@@ -577,7 +577,8 @@ class BabelUpdater:
                     },
                 )
             )
-        return '\n'.join(lines) + '\n\n'
+            lines.append('')
+        return '\n'.join(lines) + '\n'
 
     def by_domain_markdown_text(
         self,
@@ -749,6 +750,6 @@ class BabelUpdater:
             line += f'| {flagged_messages_count[flag] if flagged_messages_count[flag] else "-"} '
         if domain:
             po_file: Path = domain.locale_po_file(locale)
-            line += f'| [{po_file.name}](../../{"/".join(d.name for d in reversed(po_file.relative_to(BASE_DIR).parents))}) '
+            line += f'| [{po_file.name}](../..{"/".join(d.name for d in reversed(po_file.relative_to(BASE_DIR).parents))}/{po_file.name}) '
         line += '|'
         return line
