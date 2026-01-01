e07046c1fb0810ac83d5d84efa4af58d89baf5d6
Molrn <76754000+Molrn@users.noreply.github.com>
2026-01-01 13:08:07 +0100

    Restrict reamining unauthorized tournament access
    FFE / CR publish modal
    CE import modal
    Documents

--- src/plugins/chess_results/chess_results_event_controller.py
+++ src/plugins/chess_results/chess_results_event_controller.py
@@ -1,9 +1,12 @@
 from functools import partial
+from typing import Any
+
 from litestar import get, post
 from litestar.response import Template
 from litestar_htmx import HTMXRequest, HTMXTemplate
 
 from data.access_levels.actions import AuthAction
+from data.tournament import Tournament
 from plugins.chess_results import PLUGIN_NAME
 from plugins.chess_results.chess_results_background_uploader import (
     ChessResultsBackgroundUploader,
@@ -16,27 +19,43 @@ from web.controllers.admin.base_event_admin_controller import (
     BaseEventAdminWebContext,
 )
 from web.controllers.admin.tournament_admin_controller import TournamentAdminWebContext
-from web.guards import EventGuard, ActionGuard, TournamentActionGuard
+from web.guards import EventGuard, TournamentActionGuard
 
 get_data = partial(PluginUtils.get_plugin_data, PLUGIN_NAME)
 
 
 class ChessResultsAdminEventController(BaseEventAdminController):
-    guards = []
+    guards = [EventGuard(), TournamentActionGuard(AuthAction.PUBLISH_RESULTS)]
+
+    @staticmethod
+    def _allowed_tournaments(web_context: BaseEventAdminWebContext) -> list[Tournament]:
+        return web_context.client.allowed_tournaments_for_action(
+            AuthAction.PUBLISH_RESULTS
+        )
+
+    @classmethod
+    def _upload_results_context(
+        cls, web_context: BaseEventAdminWebContext
+    ) -> dict[str, Any]:
+        return web_context.template_context | {
+            'format_timestamp_date_time': format_timestamp_date_time,
+            'result_id': ChessResultsBackgroundUploader.result_id,
+            'upload_status_messages': ChessResultsBackgroundUploader.upload_status_messages,
+            'chess_results_utils': ChessResultsUtils,
+            'allowed_tournaments': cls._allowed_tournaments(web_context),
+        }
 
     @get(
         path='/chess-results/chess-results-upload-modal/{event_uniq_id:str}',
         name='chess-results-upload-modal',
-        guards=[EventGuard(), ActionGuard(AuthAction.PUBLISH_RESULTS)],
     )
     async def htmx_admin_chess_results_upload_modal(
-        self,
-        request: HTMXRequest,
+        self, request: HTMXRequest
     ) -> Template:
         web_context = BaseEventAdminWebContext(request)
 
         ChessResultsBackgroundUploader.update_eligible_tournaments(
-            web_context.get_admin_event()
+            self._allowed_tournaments(web_context)
         )
 
         return HTMXTemplate(
@@ -44,34 +63,22 @@ class ChessResultsAdminEventController(BaseEventAdminController):
             re_target='#modal-wrapper',
             trigger_event='modal_opened',
             after='settle',
-            context=web_context.template_context
-            | {
-                'format_timestamp_date_time': format_timestamp_date_time,
-                'result_id': ChessResultsBackgroundUploader.result_id,
-                'upload_status_messages': ChessResultsBackgroundUploader.upload_status_messages,
-                'chess_results_utils': ChessResultsUtils,
-            },
+            context=self._upload_results_context(web_context),
         )
 
-    @staticmethod
+    @classmethod
     def _render_upload_results(
+        cls,
         web_context: BaseEventAdminWebContext,
     ) -> Template:
         return HTMXTemplate(
             template_name='/chess_results_upload_results.html',
-            context=web_context.template_context
-            | {
-                'format_timestamp_date_time': format_timestamp_date_time,
-                'result_id': ChessResultsBackgroundUploader.result_id,
-                'upload_status_messages': ChessResultsBackgroundUploader.upload_status_messages,
-                'chess_results_utils': ChessResultsUtils,
-            },
+            context=cls._upload_results_context(web_context),
         )
 
     @get(
         path='/chess-results/chess-results-upload-results/{event_uniq_id:str}',
         name='chess-results-upload-results',
-        guards=[EventGuard(), ActionGuard(AuthAction.PUBLISH_RESULTS)],
     )
     async def htmx_admin_chess_results_upload_results(
         self, request: HTMXRequest
@@ -82,17 +89,17 @@ class ChessResultsAdminEventController(BaseEventAdminController):
     @post(
         path='/chess-results/chess-results-upload/{event_uniq_id:str}',
         name='chess-results-upload',
-        guards=[EventGuard(), ActionGuard(AuthAction.PUBLISH_RESULTS)],
     )
     async def htmx_admin_chess_results_upload(self, request: HTMXRequest) -> Template:
         web_context = BaseEventAdminWebContext(request)
-        ChessResultsBackgroundUploader.upload_event(web_context.get_admin_event())
+        ChessResultsBackgroundUploader.upload_event_tournaments(
+            self._allowed_tournaments(web_context)
+        )
         return self._render_upload_results(web_context)
 
     @post(
         path='/chess-results/chess-results-upload-tournament/{event_uniq_id:str}/{tournament_id:int}',
         name='chess-results-upload-tournament',
-        guards=[EventGuard(), TournamentActionGuard(AuthAction.PUBLISH_RESULTS)],
     )
     async def htmx_admin_chess_results_upload_tournament(
         self,
