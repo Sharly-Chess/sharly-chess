41e1bbfd93857c62bfcb9a0be57aa4c6936d3d1f
Molrn <76754000+Molrn@users.noreply.github.com>
2026-01-11 23:03:41 +0100

    Use column logic for player update (#1654)
    * Use column logic for player update
    
    * Review
    
    * woops
    
    ---------
    
    Co-authored-by: Pascal AUBRY <66429491+pascalaubry@users.noreply.github.com>

--- src/web/controllers/admin/player_admin_controller.py
+++ src/web/controllers/admin/player_admin_controller.py
@@ -31,7 +31,7 @@ from data.columns.player_datasheet import DatasheetColumn
 from data.event import Event
 from data.access_levels.actions import AuthAction
 from data.access_levels.client import Client
-from data.input_output.data_source import PlayerComparator, DataSource
+from data.input_output.data_source import DataSource
 from data.input_output.managers import DataSourceManager
 from data.player import Player, Federation, Club, PlayerRating, TournamentPlayer
 from data.player_categories import PlayerCategory
@@ -1793,23 +1793,23 @@ class PlayerAdminController(BaseEventAdminController):
         web_context = PlayerAdminWebContext(request, data_source_id=data_source_id)
         event = web_context.get_admin_event()
         data_source = web_context.get_admin_data_source()
-        player_updater_field_ids: list[str] = [
-            field.id for field in data_source.player_updater_fields
-        ]
-        field_ids: list[str] = [
-            field_id
-            for field_id in data['field_ids']
-            if field_id in player_updater_field_ids
+        flat_data = WebContext.flatten_list_data(data)
+        player_ids = WebContext.form_data_to_list_int(flat_data, 'player_ids')
+        field_ids = WebContext.form_data_to_list_str(flat_data, 'field_ids')
+        fields = [
+            field
+            for field in data_source.player_updater_fields
+            if field.id in field_ids
         ]
         allowed_players_by_id = web_context.client.allowed_players_by_id
         players: list[Player] = []
-        for player_id in map(int, (id_ for id_ in data['player_ids'] if id_)):
+        for player_id in player_ids:
             if player := allowed_players_by_id.get(player_id, None):
                 players.append(player)
-        player_matches = await data_source.get_player_matches(
-            players, field_ids, diff_only=True
+        player_comparators = await data_source.get_player_comparators(
+            players, fields, diff_only=True
         )
-        if player_matches is None:
+        if player_comparators is None:
             Message.error(
                 request,
                 _(
@@ -1818,10 +1818,13 @@ class PlayerAdminController(BaseEventAdminController):
                 ).format(data_source=data_source.name),
             )
         else:
-            for match in player_matches:
-                match.update_player_from_match(field_ids)
-            event.update_players([match.player for match in player_matches])
-            count: int = len(player_matches)
+            event.update_players(
+                [
+                    comparator.updated_player_from_match(fields)
+                    for comparator in player_comparators
+                ]
+            )
+            count: int = len(player_comparators)
             Message.success(
                 request,
                 ngettext(
@@ -1849,9 +1852,7 @@ class PlayerAdminController(BaseEventAdminController):
             tournament_id=tournament_id,
             data_source_id=data_source_id,
         )
-        event = web_context.get_admin_event()
         data_source = web_context.get_admin_data_source()
-        field_ids: list[str] = [field.id for field in data_source.player_updater_fields]
         players: list[Player] = []
         if tournament := web_context.admin_tournament:
             for (
@@ -1860,14 +1861,9 @@ class PlayerAdminController(BaseEventAdminController):
                 players.append(tournament_player)
         else:
             players = web_context.client.sorted_allowed_players
-        player_matches: (
-            list[PlayerComparator] | None
-        ) = await data_source.get_player_matches(
-            players,
-            field_ids,
-            diff_only=False,
-        )
-        if player_matches is None:
+        fields = data_source.player_updater_fields
+        player_comparators = await data_source.get_player_comparators(players, fields)
+        if player_comparators is None:
             Message.error(
                 request,
                 _('Could not connect to data source [{data_source}].').format(
@@ -1875,25 +1871,18 @@ class PlayerAdminController(BaseEventAdminController):
                 ),
             )
             return self._admin_event_players_render(request, reload_event=True)
-
-        per_plugin_columns: Iterable[Iterable[ExtraAdminColumn]] = (
-            plugin_manager.hook_for_event(event, 'get_extra_players_update_columns')()
-        )
-        extra_columns: dict[str, list[ExtraAdminColumn]] = {}
-        for plugin_columns in per_plugin_columns:
-            for extra_column in plugin_columns:
-                c = extra_columns.setdefault(extra_column.at, [])
-                c.append(extra_column)
-
+        updated_field_ids = {
+            field_id
+            for comparator in player_comparators
+            for field_id in comparator.diff_field_ids
+        }
         template_context = web_context.template_context | {
             'modal': 'players_diff',
             'data_source': data_source,
-            'field_ids': field_ids,
-            'player_matches': player_matches,
-            'update_enabled': any(
-                player_match.diff_field_ids for player_match in player_matches
-            ),
-            'admin_players_update_extra_columns': extra_columns,
+            'fields': fields,
+            'updated_field_ids': updated_field_ids,
+            'player_comparators': player_comparators,
+            'update_enabled': bool(updated_field_ids),
         }
         return self._admin_base_event_render(template_context)
 
