d56cdae25450c3d960306ee96438456ac0b95d56
Molrn <76754000+Molrn@users.noreply.github.com>
2026-01-20 11:12:52 +0100

    Enabled Account FIDE ID and FFE licence number

--- src/plugins/ffe/templates/ffe_account_search_result.js
+++ src/plugins/ffe/templates/ffe_account_search_result.js
@@ -1,2 +1,1 @@
-$('#ffe-licence-number-hidden').val('{{ player.plugin_data.ffe.ffe_licence_number or '' }}');
 $('#ffe-licence-number.form-control').val('{{ player.plugin_data.ffe.ffe_licence_number or '' }}');
