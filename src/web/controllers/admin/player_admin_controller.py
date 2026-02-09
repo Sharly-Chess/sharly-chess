1f5b079c01fb97608905d43a29706b85e92cc2f5
Molrn <76754000+Molrn@users.noreply.github.com>
2026-02-09 20:28:09 +0100

    Fix uncaught error on player import with unique violation (#1765)

--- src/web/controllers/admin/player_admin_controller.py
+++ src/web/controllers/admin/player_admin_controller.py
@@ -1675,7 +1675,7 @@ class PlayerAdminController(BaseEventAdminController):
                         and value
                         and value in unique_values_by_column_id[column.id]
                     ):
-                        raise ValueError(
+                        raise SharlyChessException(
                             _(
                                 'A player with this value already exists in the tournament.'
                             )
