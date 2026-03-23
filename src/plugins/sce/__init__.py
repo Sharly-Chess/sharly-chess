75b2f3469f546064b486972edcb9e1d373e40d8b
Molrn <76754000+Molrn@users.noreply.github.com>
2026-03-23 19:38:20 +0100

    Default SCE_BASE_URL to prod env

--- src/plugins/sce/__init__.py
+++ src/plugins/sce/__init__.py
@@ -6,7 +6,7 @@ from plugins import PLUGINS_DIR
 PLUGIN_NAME: str = 'sce'
 PLUGIN_DIR: Path = PLUGINS_DIR / PLUGIN_NAME
 
-SCE_BASE_URL = os.getenv('SCE_BASE_URL') or 'http://localhost:3001'
+SCE_BASE_URL = os.getenv('SCE_BASE_URL') or 'https://events.sharly-chess.com'
 SCE_SYNC_DELAY = 3
 SCE_UPLOAD_DELAY = 3
 SCE_CLIENT_ID = 'sharlychess'
