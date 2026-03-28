2d2cd87c7b301f770329911d28f531b3fbe038fb
Molrn <76754000+Molrn@users.noreply.github.com>
2026-03-28 12:46:33 +0100

    Fix event form disabled logic

--- src/plugins/sce/templates/sce_event_form_script.js
+++ src/plugins/sce/templates/sce_event_form_script.js
@@ -1,4 +1,3 @@
-
 {% set field_ids = [
     'date-range',
     'age-category-base-date',
@@ -6,18 +5,6 @@
     'age-category-change-month'
 ] %}
 
-var pluginContainer = document.getElementById('{{ plugin.form_key }}-plugin-container');
-{% for id in field_ids %}
-    {% set name = id|replace('-', '_') %}
-    var hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', '{{ name }}');
-    hiddenInput.setAttribute('id', '{{ id }}-sce-hidden');
-    hiddenInput.setAttribute('value', `{{ data[field] }}`);
-    pluginContainer.appendChild(hiddenInput);
-{% endfor %}
-
-var message = ``;
 var sceTooltipByFieldId = {};
 $('#{{ plugin.form_key }}').change(function () {
     if (this.checked) {
@@ -28,7 +15,7 @@ $('#{{ plugin.form_key }}').change(function () {
                 placement: 'top',
             });
             $('#{{ id }}').prop('disabled', true);
-            $('#{{ id }}-sce-hidden').prop('disabled', false);
+            $('#{{ id }}-hidden').prop('disabled', false);
         {% endfor %}
         $('#age-category-sets-container').hide();
     } else {
@@ -38,8 +25,11 @@ $('#{{ plugin.form_key }}').change(function () {
                 delete sceTooltipByFieldId['{{ id }}'];
             }
             $('#{{ id }}').prop('disabled', false);
-            $('#{{ id }}-sce-hidden').prop('disabled', true);
+            $('#{{ id }}-hidden').prop('disabled', true);
         {% endfor %}
+        if($('#plugin_ffe').is(':checked')) {
+            $('#plugin_ffe').trigger('change', [false]);
+        }
     }
     $('#age-category-sets-configure-button').prop('disabled', this.checked);
 });
