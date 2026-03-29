b5f868d8da86a76d814a74484b792df3d656dfd3
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2026-03-29 03:55:22 +0200

    Create POT file for new plugins Remove comments

--- scripts/i18n/i18n_translate.py
+++ scripts/i18n/i18n_translate.py
@@ -53,6 +53,9 @@ class I18nTranslator:
         domain_message_ids_to_translate: dict[str, Any] = {}
         for domain in Domain.get_domains():
             pot_file: Path = domain.pot_file
+            if not pot_file.is_file():
+                print_interactive_info(f'Creating [{pot_file}] from the sources...')
+                BabelDomainWrapper(domain.id).extract_i18n_strings()
             po_file: Path = domain.locale_po_file(self.target_locale)
             if not po_file.is_file():
                 print_interactive_info(f'Creating [{po_file}] from [{pot_file}]...')
