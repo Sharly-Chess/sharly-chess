6a310387431cb6c651599c2c100c2a03558b5aa8
Pascal AUBRY <66429491+pascalaubry@users.noreply.github.com>
2026-04-06 11:04:44 +0200

    Clean display of pairing exceptions (#1928)
    * Clean display of pairing exceptions
    
    * Raise ValueError
    Use logger.exception()
    
    * Fix error message

--- src/data/pairings/engines.py
+++ src/data/pairings/engines.py
@@ -60,18 +60,22 @@ class PairingEngine(ABC):
         tournament: 'Tournament',
         round_: int,
         partial_pairings: bool = False,
-    ) -> bool:
-        """Generate the pairings of the round *round_* for tournament *tournament*."""
+    ) -> str:
+        """Generate the pairings of the round *round_* for tournament *tournament*, returns an error message or an empty string on success."""
         if self.pairings_generation_disabled_message(tournament, round_):
             raise ValueError(
                 f'Pairings generation not allowed for round {round_} '
                 f'of tournament [{tournament.name}].'
             )
-        stored_boards = self._generate_stored_boards(
-            tournament, round_, partial_pairings
-        )
+        try:
+            stored_boards = self._generate_stored_boards(
+                tournament, round_, partial_pairings
+            )
+        except Exception as e:
+            logger.exception(e)
+            return _('An error occurred. Consult the logs for more details.')
         if len(stored_boards) == 0:
-            return False
+            return _('Pairing is not possible.')
         if self.reorder_boards:
             boards = [
                 Board(tournament, round_, stored_board)
@@ -81,7 +85,7 @@ class PairingEngine(ABC):
             for board in sorted(boards, reverse=True):
                 board.stored_board.index = available_indexes.pop(0)
         tournament.create_boards(stored_boards, round_, self.pab_result)
-        return True
+        return ''
 
     def pairings_generation_disabled_message(
         self, tournament: 'Tournament', at_round: int
