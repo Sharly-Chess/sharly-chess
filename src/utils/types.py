38a66c089a0872a31988d0db075c7cafa78463fd
Timothy Armes <tim@timothyarmes.com>
2026-09-11 08:16:00 +0200

    Store kFactor, send rating difference to Chess-Results

--- src/utils/types.py
+++ src/utils/types.py
@@ -54,6 +54,7 @@ class PlayerRating:
     estimated: int | None = None
     national: int | None = None
     fide: int | None = None
+    k_factor: int | None = None
 
     @classmethod
     def from_stored_value(cls, dict_rating: dict[str, int | None]) -> Self:
@@ -61,6 +62,7 @@ class PlayerRating:
             estimated=dict_rating.get('estimated'),
             national=dict_rating.get('national'),
             fide=dict_rating.get('fide'),
+            k_factor=dict_rating.get('k'),
         )
 
     @classmethod
@@ -101,6 +103,8 @@ class PlayerRating:
             ratings['national'] = self.national
         if self.fide is not None:
             ratings['fide'] = self.fide
+        if self.k_factor is not None:
+            ratings['k'] = self.k_factor
         return ratings
 
     def __str__(self) -> str:
